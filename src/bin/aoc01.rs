71bc0686ea72a9295f3c1d112766ccc868c23229
darksv <darek969-12@o2.pl>
2018-12-01 11:37:28 +0100

    cleanup

--- src/bin/aoc01.rs
+++ src/bin/aoc01.rs
@@ -1,9 +1,11 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = std::fs::read_to_string("inputs/input01.txt").unwrap();
     let changes = parse_input(&input);
 
-    println!("Resulting frequency = {}", changes.iter().sum::<i32>());
-    println!("First frequency occurring twice = {}", find_first_twice(&changes));
+    println!("Resulting frequency = {}", find_effective(&changes));
+    println!("First frequency occurring twice = {}", find_first_occurring_twice(&changes));
 }
 
 fn parse_input(input: &str) -> Vec<i32> {
@@ -21,9 +23,13 @@ fn parse_input(input: &str) -> Vec<i32> {
         .collect()
 }
 
-fn find_first_twice(changes: &[i32]) -> i32 {
+fn find_effective(changes: &[i32]) -> i32 {
+    changes.iter().sum::<i32>()
+}
+
+fn find_first_occurring_twice(changes: &[i32]) -> i32 {
     let mut current = 0;
-    let mut seen = std::collections::HashSet::new();
+    let mut seen = HashSet::new();
     seen.insert(current);
     for &change in changes.iter().cycle() {
         current += change;

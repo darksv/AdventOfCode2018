e20f829bf4622b87c048eb9d5c35c0bf2c4108a7
darksv <darek969-12@o2.pl>
2018-12-08 12:15:30 +0100

    add part2 for 7th day

--- src/bin/aoc07.rs
+++ src/bin/aoc07.rs
@@ -4,51 +4,46 @@ use itertools::Itertools;
 fn main() {
     let input = std::fs::read_to_string("inputs/input07.txt").unwrap();
 
-    let rules = parse_rules(input);
-    let rules = sorted_rules(&rules);
-    let sorted = find_order(&rules);
+    let edges = parse_rules(input);
+    let edges = sorted_edges(&edges);
+    let sorted = find_order(&edges);
 
     println!("Order = {}", sorted);
-    check(&rules, &sorted);
+    println!("Required time = {}", calculate_time(&edges, 5, 60));
+}
+
+#[derive(Copy, Clone)]
+struct Edge {
+    from: char,
+    to: char,
+}
+
+fn parse_rules(input: String) -> Vec<Edge> {
+    let re = regex::Regex::new(r"Step (\D+) must be finished before step (\D+) can begin.").unwrap();
+
+    input.lines().filter_map(|line| {
+        let captures = re.captures(line)?;
+        let from = captures[1].chars().next()?;
+        let to = captures[2].chars().next()?;
+        Some(Edge { from, to })
+    }).collect()
 }
 
 fn find_order(edges: &[Edge]) -> String {
     let mut sequence = String::new();
-
-    let mut visited_vertices: HashSet<char> = HashSet::new();
-
+    let mut visited: HashSet<char> = HashSet::new();
     loop {
-        let mut pending: HashSet<char> = find_roots(edges)
-            .into_iter()
-            .filter(|v| !visited_vertices.contains(v))
-            .collect();
-
-        for Edge { from, to } in edges {
-            if visited_vertices.contains(&from) && !visited_vertices.contains(&to) {
-                let ancestors_visited = edges
-                    .iter()
-                    .filter(|edge| edge.to == *to)
-                    .all(|edge| visited_vertices.contains(&edge.from));
-
-                if ancestors_visited {
-                    pending.insert(*to);
-                }
-            }
-        }
-
-
-        if let Some(v) = pending.into_iter().min() {
-            visited_vertices.insert(v);
+        if let Some(v) = get_available(edges, &visited).into_iter().min() {
+            visited.insert(v);
             sequence.push(v);
         } else {
             break;
         }
     }
-
     sequence
 }
 
-fn sorted_rules(constraints: &[Edge]) -> Vec<Edge> {
+fn sorted_edges(constraints: &[Edge]) -> Vec<Edge> {
     let mut rules = constraints.to_vec();
     rules.sort_by(|lhs, rhs|
         lhs.to.cmp(&rhs.to).then(lhs.from.cmp(&rhs.from)));
@@ -78,28 +73,78 @@ fn find_vertices(rules: &[Edge]) -> HashSet<char> {
     points
 }
 
-fn check(rules: &[Edge], order: &str) {
-    for rule in rules {
-        let p1 = order.chars().position(|x| x == rule.from).unwrap();
-        let p2 = order.chars().position(|x| x == rule.to).unwrap();
+fn calculate_time(
+    edges: &[Edge],
+    number_of_workers: u32,
+    base_time: u32
+) -> u32 {
+    let mut workers = vec![Worker::Free; number_of_workers as usize];
+    let mut elapsed = 0;
 
-        assert!(p1 < p2);
+    let mut done = HashSet::new();
+    let mut queued = HashSet::new();
+
+    loop {
+        for worker in &mut workers {
+            *worker = match *worker {
+                Worker::Working(task, remaining) if remaining > 1 => Worker::Working(task, remaining - 1),
+                Worker::Working(t, _) => {
+                    done.insert(t);
+                    Worker::Free
+                }
+                _ => Worker::Free,
+            };
+
+            if *worker == Worker::Free {
+                let available = get_available(edges, &done)
+                    .into_iter()
+                    .filter(|v| !queued.contains(v))
+                    .min();
+
+                if let Some(v) = available {
+                    *worker = Worker::Working(v, base_time + (v as u8 - b'A' + 1) as u32);
+                    queued.insert(v);
+                }
+            }
+        }
+
+        if done.len() == find_vertices(edges).len() {
+            break;
+        }
+
+        elapsed += 1;
     }
+
+    elapsed
 }
 
-fn parse_rules(input: String) -> Vec<Edge> {
-    let re = regex::Regex::new(r"Step (\D+) must be finished before step (\D+) can begin.").unwrap();
-
-    input.lines().filter_map(|line| {
-        let captures = re.captures(line)?;
-        let from = captures[1].chars().next()?;
-        let to = captures[2].chars().next()?;
-        Some(Edge { from, to })
-    }).collect()
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+enum Worker {
+    Free,
+    Working(char, u32),
 }
 
-#[derive(Copy, Clone)]
-struct Edge {
-    from: char,
-    to: char,
+fn get_available(
+    edges: &[Edge],
+    visited: &HashSet<char>
+) -> Vec<char> {
+    let mut pending: HashSet<char> = find_roots(edges)
+        .into_iter()
+        .filter(|v| !visited.contains(v))
+        .collect();
+
+    for Edge { from, to } in edges {
+        if visited.contains(&from) && !visited.contains(&to) {
+            let ancestors_visited = edges
+                .iter()
+                .filter(|edge| edge.to == *to)
+                .all(|edge| visited.contains(&edge.from));
+
+            if ancestors_visited {
+                pending.insert(*to);
+            }
+        }
+    }
+
+    pending.into_iter().collect()
 }
